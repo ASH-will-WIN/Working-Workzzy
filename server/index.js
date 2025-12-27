521775d6a49c21db5bb01487531def7ecb134716
Ashwin Shrivastav <103900575+ASH-will-WIN@users.noreply.github.com>
2025-12-26 19:13:04 -0500

    Revert "feat: Initialize server with Express, configure environment variables, CORS, and API routes for jobs, auth, payments, applications, connect, webhooks, and messages."
    This reverts commit 205974a503c3bb772063eb86b199c7eceff56d53.
    
    reverting changes that potentially broke the Wurkzi thing. OA

--- server/index.js
+++ server/index.js
@@ -23,7 +23,6 @@ const allowedOrigins = [
   "https://fantastic-motivation-production.up.railway.app",
   "https://workzzyunos.onrender.com",
   "http://localhost:3000",
-  "https://wurkzi.com"
 ];
 
 if (process.env.CLIENT_URL) {
