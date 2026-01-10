d7007128f82ac47bdafe858408cdde005bb8c3bf
Ashwin Shrivastav <103900575+ASH-will-WIN@users.noreply.github.com>
2026-01-10 16:18:24 -0500

    init ios commit w prod env

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.workzzy.app',
+  appName: 'Workzzy',
+  webDir: 'build'
+};
+
+export default config;
